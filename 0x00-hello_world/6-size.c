408a809c107be3a8e7fddc22d3c00a93a7bddc3b
onebyte2016 <onebyte2016@gmail.com>
2023-08-10 11:32:15 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float f;
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of long int: %lu byte(s)\n", sizeof(i));
-printf("Size of long long int: %lu byte(s)\n", sizeof(i));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
