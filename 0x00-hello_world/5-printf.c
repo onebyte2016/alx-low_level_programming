a50e514d07cffe0977c3faa999a681cc7c5e8a8e
onebyte2016 <onebyte2016@gmail.com>
2023-08-10 09:38:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function body
+ *
+ * Return: end the function with 0
+ */
+int main(void)
+{
+printf( "with proper grammar, but the outcome is a piece of art ,");
+return (0);
+}
