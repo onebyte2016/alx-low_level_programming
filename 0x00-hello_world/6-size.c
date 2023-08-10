f43886034f93beb8c717bb08c68011420e4bae18
onebyte2016 <onebyte2016@gmail.com>
2023-08-10 11:55:10 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,19 @@
  * main - the begining of the main function
  *
  * Description: using sizeof to print the size of various type
- i*
+ *
  * Return: always 0 success
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+int i;
+char c;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of long int: %lu byte(s)\n", sizeof(i));
 printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
