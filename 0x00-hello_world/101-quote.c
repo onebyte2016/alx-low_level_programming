d7c0ee0d4c811ad89e5ff5683f3fbd2af011313e
onebyte2016 <onebyte2016@gmail.com>
2023-08-10 12:23:50 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - the begnining of the  main function
+ *
+ * Return: return 1 as required
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+return (1);
+}
