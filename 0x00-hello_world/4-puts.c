66d709c6a4966eaee0d69ca320bab1f97a8d4899
onebyte2016 <onebyte2016@gmail.com>
2023-08-10 09:11:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - contains the main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
 return (0);
 }
-
