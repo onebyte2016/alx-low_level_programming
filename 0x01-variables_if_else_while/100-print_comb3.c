cb4e3ac521d54facc10a58cb3a6541ba1b802cd6
onebyte2016 <onebyte2016@gmail.com>
2023-08-11 11:35:51 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - the start of the program main function
+*
+* Return: Always return zero
+*
+* betty style doc for function main goes there
+*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8 || j < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
