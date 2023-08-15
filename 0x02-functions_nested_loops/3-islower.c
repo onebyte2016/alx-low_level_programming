0ca7d066c5ed2e2e6233a957c03ef127e5eea8fa
onebyte2016 <onebyte2016@gmail.com>
2023-08-15 14:25:21 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _islower - It compares lower case letter
+*
+* Return: Always 0
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
