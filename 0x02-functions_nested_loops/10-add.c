782d65c841d392527addb472480233e72f417133
onebyte2016 <onebyte2016@gmail.com>
2023-08-15 20:03:24 -0700

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - function add two integers together
+* @a: inter variable 1.
+* @b: integer variable 2.
+*
+* Return: Always 0.
+*/
+
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
