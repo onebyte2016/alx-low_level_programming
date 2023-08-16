afd6b5e349ca7ea62fa010d11405d6595825d995
onebyte2016 <onebyte2016@gmail.com>
2023-08-16 04:36:14 -0700

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,9 @@ int numlenght(int num)
 {
 int lenght = 0;
 if (!num)
+{
 return (1);
+}
 while (num)
 {
 num = num / 10;
@@ -38,7 +40,9 @@ unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 for (count = 1; count <= 98; count++)
 {
 if (f1o > 0)
+{
 printf("%lu", f1o);
+}
 initial0s = numlenght(mx) - 1 - numlenght(f1);
 while (f1o > 0 && initial0s > 0)
 {
@@ -53,10 +57,13 @@ f1o = f2o;
 f2 = sum;
 f2o = sumo;
 if (count != 98)
-printf(",");
+{
+printf(", ");
+}
 else
+{
 printf("\n");
-
+}
 }
 return (0);
 }
