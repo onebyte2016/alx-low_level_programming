1acd1799bbdd90645840b31664be837796595b62
onebyte2016 <onebyte2016@gmail.com>
2023-08-15 21:49:42 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int sum, num;
+for (num = 0; num < 1024; num++)
+{
+if ((num % 3 == 0) || (num % s == 0))
+sum += sum;
+
+}
+printf("%d\n", sum);
+return (0);
+}
