0b6ef751e7e2b5a06c7c504a0c901434da62e3ec
onebyte2016 <onebyte2016@gmail.com>
2023-08-17 09:08:18 -0700

    printing upper case letere

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _isupper - print upper case letters.
+ *
+ * @c: parameter passed through the function isupper.
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
