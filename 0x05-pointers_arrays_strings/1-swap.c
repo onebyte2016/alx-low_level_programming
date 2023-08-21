73613e8cfeb8c0b748db780292b04aa299d35068
onebyte2016 <onebyte2016@gmail.com>
2023-08-21 11:44:44 -0700

    swap values of integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - function that swap two integers a and b
+ *
+ * @a: parameter for the first argument
+ *
+ * @b: Parameter for the second argument.
+ */
+
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
