5f1bd8a31195880bdf5b0530f82e338bb431b4af
onebyte2016 <onebyte2016@gmail.com>
2023-08-21 15:32:14 -0700

    print string using puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - function for printing string
+ *
+ * @str: parameter passed.
+ */
+
+void puts_half(char *str)
+{
+	int stringLength = strlen(str);
+	int begin_index, i;
+
+	if (stringLength % 2 == 0)
+	{
+		begin_index = stringLength / 2;
+	}
+	else
+	{
+		begin_index = (stringLength - 1) / 2;
+	}
+
+	for (i = begin_index; i < stringLength; i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
