c117de547369d41ecfae81e3e896a5b72e4c8c77
onebyte2016 <onebyte2016@gmail.com>
2023-08-21 14:16:34 -0700

    reverse printing of string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - function for reverse printing of string
+ *
+ * @s: parameter passed to the function
+ */
+
+
+
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int start = 0;
+	int end = length - 1;
+
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
