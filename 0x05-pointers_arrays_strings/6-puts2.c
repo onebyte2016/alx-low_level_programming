a89734bdbbffd35b9fb05fe1a0d354d9356df2eb
onebyte2016 <onebyte2016@gmail.com>
2023-08-21 14:39:08 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - function to print string
+ *
+ * @str: parameter passed
+ */
+
+
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}
