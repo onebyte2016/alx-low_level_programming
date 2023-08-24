12bcd521c607596c7bf568ff198ae6e244ab19bc
onebyte2016 <onebyte2016@gmail.com>
2023-08-24 08:19:32 -0700

    100 rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,29 +6,25 @@
  * @s: first parameter
  *
  *
- * Return: ptr
+ * Return: return the value of s
  */
-
 
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
+	char *a = "abcdefghijknmlopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	char rot13[] = "abcdefghijknmlopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *ptr = s;
-
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i <= 52; i++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (*s == rot13[i])
+			if (s[i] == a[j])
 			{
-				*s = ROT13[i];
+				s[i] = b[j];
 				break;
 			}
 		}
-		s++;
 	}
-	return (ptr);
+	return (s);
 }
