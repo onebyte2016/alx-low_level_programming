d0003bd5da6bd22de916db3b909540e461dfa134
onebyte2016 <onebyte2016@gmail.com>
2023-08-24 00:18:59 -0700

    reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - function to concantenate string
+ * @a: first parameter
+ *
+ * @n: second parameter
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+	for (i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+
+	}
+}
