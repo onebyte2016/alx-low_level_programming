ee4e332e7ac97b9c610c25ad21555a381f6a81ce
onebyte2016 <onebyte2016@gmail.com>
2023-08-23 22:54:29 -0700

    concantenate string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * _strcat - function to concantenete string
+ * @dest: first parameter
+ * @src: second parameter
  *
- * Return: Always 0.
+ * Return: returning dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
-	c =0;
-	while(dest[c])
+
+	c = 0;
+	while (dest[c])
 		c++;
 	for (c2 = 0; src[c2]; c2++)
-		dest[c++] = scr[c2];
+		dest[c++] = src[c2];
 	return (dest);
 }
