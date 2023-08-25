16b205a1ae688f3d2709ea894e06eaaca4ff4d8c
onebyte2016 <onebyte2016@gmail.com>
2023-08-24 17:26:40 -0700

    a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,25 +7,55 @@
  *
  *
  * Return: return the value of s
+ *
+
+ char *rot13(char *s)
+ {
+ int i, j;
+
+ char a[] = "abcdefghijknmlopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+ for (i = 0; s[i] != '\0'; i++)
+ {
+ for (j = 0; a[j] != '\0'; j++)
+ {
+ if (s[i] == a[j])
+ {
+ s[i] = b[j];
+ break;
+ }
+ }
+ }
+ return (s);
+ }*/
+
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: argument passed
+ *
+ * Return: result
  */
 
 char *rot13(char *s)
 {
-	int i, j;
+	int x, y;
 
-	char a[] = "abcdefghijknmlopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		for (y = 0; a[y] != '\0'; y++)
 		{
-			if (s[i] == a[j])
+			if (s[x] == a[y])
 			{
-				s[i] = b[j];
+				s[x] = b[y];
 				break;
 			}
 		}
 	}
-	return (s);
+
+	     return (s);
 }
