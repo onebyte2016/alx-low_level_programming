0e9c008a3f90bd2e1d8d0340adb2c6d7578088cd
onebyte2016 <onebyte2016@gmail.com>
2023-08-28 16:00:15 -0700

    a function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - copy memory for src to dest
+ * @a: the address of memory to copy
+ * Return: dest.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
