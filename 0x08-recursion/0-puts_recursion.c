1e07d6b24c34e4e71fa3d2d65bb197289f91a12b
onebyte2016 <onebyte2016@gmail.com>
2023-08-30 06:36:31 -0700

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string
+ * @s:character parameter
+ *
+ * Return: Always 0.
+ */
+
+
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+
+	}
+
+}
