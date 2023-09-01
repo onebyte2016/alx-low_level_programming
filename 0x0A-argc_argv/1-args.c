35bde49ddc926f4e477a75aa4404da88aae6b34b
onebyte2016 <onebyte2016@gmail.com>
2023-09-01 15:12:27 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * @argc: first argument
+ * @argv: second argumet
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("argc == %d\n", argc);
+	return (0);
+
+}
