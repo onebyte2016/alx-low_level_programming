a7b1cbab3cf393821168e2cfc723ddaa97e6f1da
onebyte2016 <onebyte2016@gmail.com>
2023-09-01 17:03:27 -0700

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: Always 0
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+
+	}
+	else
+	{
+
+		printf("Error\n");
+		return (1);
+
+	}
+	return (0);
+
+
+}
