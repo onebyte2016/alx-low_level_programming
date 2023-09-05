ef61b8b44c451ca9ced71fa9b4e6e95ae898c05f
onebyte2016 <onebyte2016@gmail.com>
2023-09-05 07:39:34 -0700

    create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * create_array - function to create array
+ * @size: first parameter
+ * @c: second parameter
+ * Return: always 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int j;
+	char *new;
+
+	if (size == 0)
+		return (NULL);
+
+	new = malloc(sizeof(char) * size);
+	if (new == NULL)
+		return (NULL);
+
+	for (j = 0; j < size; j++)
+	{
+		new[j] = c;
+	}
+	return (new);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+the readme page for malloc and free
