b801c2b30560f6c9a8e548088114abbff9208f4c
onebyte2016 <onebyte2016@gmail.com>
2023-09-05 08:14:19 -0700

    strint pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - function to return pointer to string
+ * @str: first parameter
+ * Return: Always
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int count = 0;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	copy = malloc(sizeof(char) * count + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < str[i] != 0; i++)
+		copy[i] = str[i];
+	return (copy);
+}
