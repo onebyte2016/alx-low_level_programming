7f8068ce925ecbac3848ab5f83ddc0ab59b104fe
onebyte2016 <onebyte2016@gmail.com>
2023-09-11 19:22:53 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - check the code
+ * @name: parameter
+ * @age: second parameter
+ * @owner: third parameter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
