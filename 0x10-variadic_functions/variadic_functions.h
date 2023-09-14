6635d384d982a6c46788cd920e40f6b0bca7111b
onebyte2016 <onebyte2016@gmail.com>
2023-09-14 15:44:49 -0700

    function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,28 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
+#ifndef VARIADIC_H
+#define VARIADIC_H
 
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* struct token - token
+* @token: format token
+* @f: function associate
+*/
+
+typedef struct token
+{
+  char *token;
+void (*f)(char *, va_list);
+} token_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void format_string(char *separator, va_list ap);
+void format_float(char *separator, va_list ap);
+void format_int(char *separator, va_list ap);
+void format_char(char *separator, va_list ap);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
