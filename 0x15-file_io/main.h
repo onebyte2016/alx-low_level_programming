658f54ef3e0978a3383d8fc17e2169db974a0498
onebyte2016 <onebyte2016@gmail.com>
2023-10-02 17:16:44 -0700

    reat text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,11 @@
 
 #include <stadio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
